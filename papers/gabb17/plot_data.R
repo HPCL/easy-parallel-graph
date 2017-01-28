aa1efe23a510da0469de7a5c606dfc71e774ae94
Samuel Pollard <sam.d.pollard@gmail.com>
2017-01-28 00:22:53 -0800

    Added more plots and plot-generation R code.
    Also updated some littel bits fo the writeup and some more TODOS.
    Makefile now points to the graphics directory but I didn't add any
    graphics to the repo yet since they most certainly will change.
    Removed execute permission on IEEtran.cls.

--- papers/gabb17/plot_data.R
+++ papers/gabb17/plot_data.R
@@ -1,13 +1,76 @@
 # This is some bad bad bad R code.
 # Might want to double check it.
 scale <- 20
+bpc <- "chartreuse"
 filename <- paste0("parsed",scale,".csv")
 x <- read.csv(filename, header = FALSE)
-bfs_time <- subset(x, x[[2]] == "BFS" & x[[3]] == "Time", c(V1,V4))
-colnames(bfs_time) <- c("System","Time")
-# Boxplot log = "y" may be more useful.
-# TODO: Figure out why the times are so low sometimes!
-boxplot(Time~System, bfs_time, ylab = "Time (seconds)")
+colnames(x) <- c("Sys","Algo","Metric","Time")
+
+bfs_time <- subset(x, x$Algo == "BFS" & x$Metric == "Time",
+		c("Sys","Time"))
+bfs_dsc <- subset(x, x$Algo == "BFS" & x$Metric == "Data structure build",
+		c("Sys","Time"))
+bfs_dsc$Sys <- factor(bfs_dsc$Sys)
+
+sssp_time <- subset(x, x$Algo == "SSSP" & x$Metric == "Time",
+		c("Sys","Time"))
+sssp_dsc <- subset(x, x$Algo == "SSSP" & x$Metric == "Data structure build",
+		c("Sys","Time"))
+sssp_time$Sys <- factor(sssp_time$Sys)
+sssp_dsc$Sys <- factor(sssp_dsc$Sys)
+
+pr_time <- subset(x, x$Algo == "PageRank" & x$Metric == "Time",
+		c("Sys","Time"))
+pr_dsc <- subset(x, x$Algo == "PageRank" & x$Metric == "Data structure build",
+		c("Sys","Time"))
+pr_iters <- subset(x, x$Algo == "PageRank" & x$Metric == "Iterations",
+		c("Sys","Time"))
+pr_time$Sys <- factor(pr_time$Sys)
+pr_dsc$Sys <- factor(pr_dsc$Sys)
+
+pdf("graphics/bfs_time.pdf")
+boxplot(Time~Sys, bfs_time, ylab = "Time (seconds)",
+		main = "BFS Time", log = "y", col=bpc)
+dev.off()
+
+pdf("graphics/bfs_dsc.pdf")
+boxplot(Time~Sys, bfs_dsc, ylab = "Time (seconds)",
+		main = "BFS Data Structure Construction", col=bpc, log = "y")
+dev.off()
+
+pdf("graphics/sssp_time.pdf")
+boxplot(Time~Sys, sssp_time, ylab = "Time (seconds)",
+		main = "SSSP Time", log = "y", col=bpc)
+dev.off()
+
+pdf("graphics/sssp_dsc.pdf")
+boxplot(Time~Sys, sssp_time, ylab = "Time (seconds)",
+		main = "SSSP Data Structure Construction", log = "y", col=bpc)
+dev.off()
+
+pdf("graphics/pr_time.pdf")
+barplot(pr_time$Time, ylab = "Time (seconds)",
+		main = "PageRank Time", log = "y", col=bpc,
+		names.arg=factor(pr_time$Sys))
+dev.off()
+
+pdf("graphics/pr_iters.pdf")
+barplot(pr_iters$Time, ylab = "Time (seconds)",
+		main = "PageRank Iterations", col="brown",
+		names.arg=factor(pr_time$Sys))
+dev.off()
+
+# Try out a violin plot too!
+# Maybe bisque isn't the best color for the curvy plots.
+# For extra fun, log-transform the data (violins are made of wood)
+library(vioplot)
+# c("GraphBIG","GraphMat","GAP")
+bfs_t_gb <- bfs_time[["Time"]][bfs_time[["Sys"]] == "GraphBIG"]
+bfs_t_gm <- bfs_time[["Time"]][bfs_time[["Sys"]] == "GraphMat"]
+bfs_t_gap <- bfs_time[["Time"]][bfs_time[["Sys"]] == "GAP"]
+vioplot(bfs_t_gb, bfs_t_gm, bfs_t_gap,
+		names=c("GraphBIG", "GraphMat", "GAP"),
+		col="bisque")
 
 # File reading not as useful
 file_reading <- subset(x, x[[3]] == "File reading", select = c(V1,V4))
@@ -15,9 +78,6 @@ file_reading_times <- c(0,0,0)
 file_reading_times[1] <- mean(file_reading[[2]][1:3])
 file_reading_times[2] <- mean(file_reading[[2]][4:6])
 file_reading_times[3] <- mean(file_reading[[2]][7:9])
-
-ds_const <- subset(x, x[[3]] == "BFS" & x[[3]] == "Data structure build",
-		select = c(V1,V4))
 
 # This doesn't work well
 #barplot(file_reading_times,
