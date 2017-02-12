c857a9d5917c06b79da36094b35e9a16c35dac3b
Sam Pollard <spollard@cs.uoregon.edu>
2017-02-11 16:47:23 -0800

    Updated merge-csv and runtime for GraphMat.

--- papers/report/merge-csv.sh
+++ papers/report/merge-csv.sh
@@ -4,6 +4,8 @@
 # Grabs all of the runs for a given dataset and averages them.
 # To specify: The EXPERIMENT_DIR directory specicies where the output is saved.  
 EXPERIMENT_DIR=/home/users/spollard/graphalytics/experiments
+PLATFORMS="openg powergraph graphmat"
+
 first=1
 outfile=runtime.csv
 N_EXPERIMENTS=0
@@ -11,14 +13,13 @@ if [ -z "$EXPERIMENT_DIR" ]; then
 	echo "Please specify where the output csv files are located."
 	exit 1
 fi
-PLATFORMS="openg powergraph"
-# Compute the mean runtime.
+# Compute the mean runtime for each platform and store the results in the cwd
 for p in $PLATFORMS; do
 	P_RUNS=$(find "$EXPERIMENT_DIR" -path */"${p}"-report-*/runtime.csv)
 	awk -F ',' '/^,/{if (ARGIND==1) print $0} !/^,/{A[$1] += $2} END{for (alg in A) print alg "," (A[alg] / ARGIND)}' $P_RUNS > "${p}-runtime.csv"
-	N_EXPERIMENTS=$(awk 'BEGIN{print ARGC}' $P_RUNS)
+	N_EXPERIMENTS=$(awk 'BEGIN{print ARGC - 1}' $P_RUNS)
+	echo "$p has $N_EXPERIMENTS experiment(s)"
 done
-# Assumes everything is in the same directory named "openg-runtime.csv" etc.
 # Assumes there is only one dataset---named $dataset1.
 echo "Writing"
 for f in *-runtime.csv; do
@@ -32,5 +33,5 @@ for f in *-runtime.csv; do
 		cat "$f" | cut -d ',' -f 2- | sed "s/$dataset1/$experimentn/" | paste -d, runtime.csv - | tee "$outfile"
  	fi
 done
-echo "to $outfile using dataset $dataset1 over the course of $N_EXPERIMENTS runs"
+echo "to $outfile using dataset $dataset1"
 
