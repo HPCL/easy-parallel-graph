92a9548f7b7aae733c8baae910b1dada077e2314
Sam Pollard <spollard@cs.uoregon.edu>
2017-03-16 14:06:13 -0700

    Added example autoperf for mst. Not quite working.

---
+++ autoperf/mst/test_db.py
@@ -0,0 +1,18 @@
+# Test a connection with a database.
+# If it doesn't exist, suggest a way to create it.
+
+import psycopg2
+# Check if database exists
+conn_string = "dbname='epgdb' user='demo_user' password='demo_pwd' host='brix.d.cs.uoregon.edu'"
+try:
+    conn = psycopg2.connect(conn_string)
+except psycopg2.Error as ex:
+    print("{}: {}".format(ex.__class__.__name__, ex))
+    print("Unable to connect to database. Try creating a database with the following:")
+    print("createdb -h localhost -p 5432 -O taudb -U demo_user epgdb")
+    print("You may want to create a new user so that people who aren't you can access your database.")
+    print("su; useradd demo_user; passwd demo_user; # Make password demo_pwd")
+    print("If you mess up, just do dropdb -h localhost -p 5432 -O taudb -U demo_user epgdb")
+else:
+    print("Database connection using\n{}\nalready established".format(conn_string))
+    conn.close()
