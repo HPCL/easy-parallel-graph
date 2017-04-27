6d8676c03093c147479c6651e86e631ab5fcc51b
Sam Pollard <spollard@cs.uoregon.edu>
2017-04-27 01:13:50 -0700

    Updated real-data and gen-data, mostly untested.
    The idea is to have gen-datasets do the conversion both on synthetic
    and real-world, while real-datasets actually running the experiment.

--- experiment/gen-datasets.sh
+++ experiment/gen-datasets.sh
@@ -5,23 +5,25 @@
 # This only needs to be done once per scale.
 # For example, with S=20 the structure will look like
 # $DDIR/
-#   kron-20.graph500
-#   kron-20.roots
-#   kron-20-roots.v
-#   kron-20.el
-#   kron-20-undir.el
 #   kron-20/
+#     kron-20.graph500
+#     kron-20.roots
+#     kron-20-roots.v
+#     kron-20.el
+#     kron-20-undir.el
+#     kron-20/
 #     vertex.csv
 #     edge.csv
+# And likewise for the real datasets, replacing 'kron-20' as appropriate.
 
 # NOTE: It is assumed the libraries in LIBDIR are already built.
 #       This requires Graph500, GraphMat, and GAP
 USAGE="usage: gen-datasets.sh [--libdir=<dir>] [--ddir=<dir>] <scale>
+	<scale> may be either an integer or the string 'real'
 	--libdir: repositories directory. Default: ./lib
 	--ddir: dataset directory. Default: ./datasets" # 2^{<scale>} = Number of vertices.
 if [ -z "$1" ]; then
-	echo $USAGE
-	echo 'You may also need to set the variables *DIR in gen-datasets.sh'
+	echo "$USAGE"
 	exit 2
 fi
 DDIR="$(pwd)/datasets" # Dataset directory
@@ -49,35 +51,78 @@ if [ "$#" -lt 1 ]; then
 	echo $USAGE
 	exit 2
 fi
-S=$1
+case $1 in
+[1-9]|[1-9][0-9]*) # scale between 1--99 is robust enough
+	S=$1
+	DATA_PREFIX="kron-$S"
+	REAL='no'
+;;
+real*)
+	DATA_PREFIX="dota-league cit-Patents"
+	REAL='yes'
+;;
+*) # Default
+	echo "<scale> must be either a positive number or 'real' (without quotes)"
+	exit 2
+;;
+esac
+
+export OMP_NUM_THREADS=32
 GAPDIR="$LIBDIR/gapbs"
 GRAPH500DIR="$LIBDIR/graph500"
 GRAPHMATDIR="$LIBDIR/GraphMat"
-BASE_FN="kron-$S"
+if [ "$REAL" = 'yes' ]; then
+	for d in $DATA_PREFIX; do
+		echo Converting $d into the correct formats...
+		mkdir -p "$DDIR/$d"
+		# We convert to an integer because some systems store weights that way
+		# We add 1 for GraphMat to ensure it's 1-indexed (adding 1 won't hurt)
+		awk '{printf "%d %d\n", ($1+1), ($2+1)}' "$DDIR/$d.e" > "$DDIR/$d/$d.1wel"
+		if [ $(awk '{print NF; exit}' "$DDIR/$d.e") -eq 2 ]; then
+			echo "SRC,DEST" > "$DDIR/$d/edge.csv"
+		elif [ $(awk '{print NF; exit}' "$DDIR/$d.e") -eq 3 ]; then
+			echo "SRC,DEST,WEIGHT" > "$DDIR/$d/edge.csv"
+		else
+			echo "File format not recognized"
+			exit 1
+		fi
+		sed 's/[:space:]+/,/' "$DDIR/$d.e" >> "$DDIR/$d/edge.csv"
+		echo "ID" > "$DDIR/$d/vertex.csv"
+		sed 's/[:space:]+/,/' "$DDIR/$d.v" >> "$DDIR/$d/vertex.csv"
+		# Get the roots by running SSSP and making sure you can visit a good
+		# number of vertices
+		"$GAPDIR/sssp" -f "$DDIR/$d/$d.1wel" -n $(($NRT*2)) > tmp.log
+		# GAP output is e.g. "took 382 iterations".
+		# We don't want it to take 1 iteration.
+		awk -v NRT=$NRT '/Source/{src=$2}/took [0-9]+ iterations/{if($2>1 && cnt<NRT){printf "%d\n", src; cnt++}}' tmp.log > "$DDIR/$d/$d-roots.v"
+		rm tmp.log
+	done
+else # synthetic
+	# Generate graph (Graph500 can only save to its binary format)
+	d="$DATA_PREFIX"
+	mkdir -p "$DDIR/$BASE_FN"
+	"$GRAPH500DIR/make-edgelist" -s $S -o "$DDIR/$BASE_FN.graph500" -r "$DDIR/$BASE_FN.roots"
+	# Convert to GAP format (edgelist)
+	"$GRAPH500DIR/graph5002el" "$DDIR/$BASE_FN.graph500" "$DDIR/$BASE_FN.roots" "$DDIR/$BASE_FN.el" "$DDIR/${BASE_FN}-roots.v"
+	# Sort the roots (mitigate that weird issue with GraphBIG not working for root > # vertices read)
+	cat "$DDIR/${BASE_FN}-roots.v" | sort -n > tmp.txt
+	cp tmp.txt "$DDIR/${BASE_FN}-roots.v"
+	rm tmp.txt
 
-# Generate graph (Graph500 can only save to its binary format)
-"$GRAPH500DIR/make-edgelist" -s $S -o "$DDIR/$BASE_FN.graph500" -r "$DDIR/$BASE_FN.roots"
-# Convert to GAP format (edgelist)
-"$GRAPH500DIR/graph5002el" "$DDIR/$BASE_FN.graph500" "$DDIR/$BASE_FN.roots" "$DDIR/$BASE_FN.el" "$DDIR/${BASE_FN}-roots.v"
-# Sort the roots (mitigate that weird issue with GraphBIG not working for root > # vertices read)
-cat "$DDIR/${BASE_FN}-roots.v" | sort -n > tmp.txt
-cp tmp.txt "$DDIR/${BASE_FN}-roots.v"
-rm tmp.txt
+	# Symmetrize (make undirected)
+	"$GAPDIR/converter" -f "$DDIR/${BASE_FN}.el" -s -e "$DDIR/${BASE_FN}-undir.el"
+	# Convert to GraphBIG format
+	awk 'BEGIN{print "SRC,DEST"} {printf "%d,%d\n", $1, $2}' "$DDIR/${BASE_FN}-undir.el" > "$DDIR/$BASE_FN/edge.csv"
+	echo ID > "$DDIR/$BASE_FN/vertex.csv"
+	cat "$DDIR/${BASE_FN}-undir.el" | tr ' ' '\n' | sort -n | uniq >> "$DDIR/$BASE_FN/vertex.csv"
 
-# Symmetrize (make undirected)
-"$GAPDIR/converter" -f "$DDIR/${BASE_FN}.el" -s -e "$DDIR/${BASE_FN}-undir.el"
-# Convert to GraphBIG format
-mkdir -p "$DDIR/$BASE_FN"
-awk 'BEGIN{print "SRC,DEST"} {printf "%d,%d\n", $1, $2}' "$DDIR/${BASE_FN}-undir.el" > "$DDIR/$BASE_FN/edge.csv"
-echo ID > "$DDIR/$BASE_FN/vertex.csv"
-cat "$DDIR/${BASE_FN}-undir.el" | tr ' ' '\n' | sort -n | uniq >> "$DDIR/$BASE_FN/vertex.csv"
-
-# Convert to GraphMat format
-# GraphMat requires edge weights---Just make them all 1 for the .1wel format
-# XXX: What happens when you remove selfloops and duplicated edges.
-awk '{printf "%d %d %d\n", ($1+1), ($2+1), 1}' "$DDIR/${BASE_FN}.el" > "$DDIR/$BASE_FN.1wel"
-awk '{printf "%d\n", ($1+1)}' "$DDIR/${BASE_FN}-roots.v" > "$DDIR/${BASE_FN}-roots.1v"
-"$GRAPHMATDIR/bin/graph_converter" --selfloops 1 --duplicatededges 0 --bidirectional --inputformat 1 --outputformat 0 --inputheader 0 --outputheader 1 --inputedgeweights 1 --outputedgeweights 2 --nvertices $(cat $DDIR/${BASE_FN}.1wel | wc -l) "$DDIR/$BASE_FN.1wel" "$DDIR/$BASE_FN.graphmat"
+	# Convert to GraphMat format
+	# GraphMat requires edge weights---Just make them all 1 for the .1wel format
+	# XXX: What happens when you remove selfloops and duplicated edges.
+	awk '{printf "%d %d %d\n", ($1+1), ($2+1), 1}' "$DDIR/${BASE_FN}.el" > "$DDIR/$BASE_FN.1wel"
+	awk '{printf "%d\n", ($1+1)}' "$DDIR/${BASE_FN}-roots.v" > "$DDIR/${BASE_FN}-roots.1v"
+	"$GRAPHMATDIR/bin/graph_converter" --selfloops 1 --duplicatededges 0 --bidirectional --inputformat 1 --outputformat 0 --inputheader 0 --outputheader 1 --inputedgeweights 1 --outputedgeweights 2 --nvertices $(cat $DDIR/${BASE_FN}.1wel | wc -l) "$DDIR/$BASE_FN.1wel" "$DDIR/$BASE_FN.graphmat"
+fi
 
 # TEST: Convert back to non-binary, see what we get
 # "$GRAPHMATDIR/bin/graph_converter" --selfloops 1 --duplicatededges 1 --inputformat 0 --outputformat 1 --inputheader 1 --outputheader 1 --inputedgeweights 1 --outputedgeweights 2 "$DDIR/$BASE_FN.graphmat" "$DDIR/$BASE_FN.test.1wel"
--- experiment/real-datasets.sh
+++ experiment/real-datasets.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
-# Runs all the experiments for a given thread count on the available datasets
-# Current algorithms:
-#   Breadth First Search, Single Source Shortest Paths, PageRank
-# Current platforms:
-#   GraphBIG, Graph500, GAP, GraphMat, PowerGraph (SSSP & PR only)
-# Recommended usage for bash
-#   ./run-experiment $T > outreal-${T}.log 2> outreal-${T}.err &
-#   disown %<jobnum> # This can be found out using jobs
-USAGE="usage: real-datasets.sh <num-threads>"
+USAGE="usage: real-datasets.sh [--libdir=<dir>] [--ddir=<dir>] <num_threads>
+	--libdir: repositories directory. Default: ./lib
+	--ddir: dataset directory. Default: ./datasets"
 
-# Say where the packages are located
-# NOTE: YOU MUST SET THE REPOSITORY LOCATIONS AND MODULES TO LOAD
-# It is assumed these are all built and the gen-dataset.sh script has been run.
-# The building instructions are available in the respective repositories.
-# Here, edge factor (number of edges per vertex) is the default of 16.
-DDIR=
-GAPDIR=
-GRAPHBIGDIR=
-GRAPH500DIR=
-GRAPHMATDIR=
-POWERGRAPHDIR=
+DDIR="$(pwd)/datasets" # Dataset directory
+LIBDIR="$(pwd)/lib"
+for arg in "$@"; do
+	case $arg in
+	--libdir=*)
+		LIBDIR=${arg#*=}
+		shift
+	;;
+	--ddir=*)
+		DDIR=${arg#*=}
+		shift
+	;;
+	-h|--help|-help)
+		echo "$USAGE"
+		exit 2
+	;;
+	*)	# Default
+		# Do nothing
+	esac
+done
+if [ "$#" -lt 1 ]; then
+	echo 'Please provide <num_threads>'
+	echo $USAGE
+	exit 2
+fi
 
 # Choose the datasets. This is a space-separated list.
-DATA="dota-league"
+DATA="dota-league cit-Patents"
 
-# Set some other variables used throughout the experiment
-# PageRank is usually represented as a 32-bit float,
-# so ~6e-8*nvertices is the minimum absolute error detectable
-# We set alpha = 0.15 in the respective source codes.
-# NOTE: GraphMat doesn't seem to compute iterations in the same way.
+# The reasoning behind these values are explained in run-experiment.sh
 MAXITER=50 # Maximum iterations for PageRank
 TOL=0.00000006
 NRT=32 # Number of roots
@@ -38,41 +42,17 @@ if [ "$#" -ne 1 -o "$1" = "-h" -o "$1" = "--help" ]; then
 	exit 2
 fi
 export OMP_NUM_THREADS=$1
+GAPDIR="$LIBDIR/gapbs"
+GRAPHBIGDIR="$LIBDIR/graphBIG"
+GRAPH500DIR="$LIBDIR/graph500"
+GRAPHMATDIR="$LIBDIR/GraphMat"
+POWERGRAPHDIR="$LIBDIR/PowerGraph"
 
-# Load all the modules here
+# icpc required for GraphMat
 module load intel/17
 
 # Build notes for arya:
 # see run-experiment.sh
-
-# echo Converting $DATA into the correct formats...
-# # We convert to an integer because some systems store weights that way
-# We add 1 for GraphMat to ensure it's 1-indexed (adding 1 won't hurt)
-awk '{printf "%d %d\n", ($1+1), ($2+1)}' "$DDIR/$DATA.e" > "$DDIR/$DATA.wel"
-# Transform datasets from Graphalytics format to GraphBIG format
-# This has also been tested with cit-Patents
-for d in $DATA; do
-	mkdir -p "$DDIR/$d"
-	if [ $(awk '{print NF; exit}' "$DDIR/$d.e") -eq 2 ]; then
-		echo "SRC,DEST" > "$DDIR/$d/edge.csv"
-	elif [ $(awk '{print NF; exit}' "$DDIR/$d.e") -eq 3 ]; then
-		echo "SRC,DEST,WEIGHT" > "$DDIR/$d/edge.csv"
-	else
-		echo "File format not recognized"
-		exit 1
-	fi
-	sed 's/[:space:]+/,/' "$DDIR/$d.e" >> "$DDIR/$d/edge.csv"
-	echo "ID" > "$DDIR/$d/vertex.csv"
-	sed 's/[:space:]+/,/' "$DDIR/$d.v" >> "$DDIR/$d/vertex.csv"
-done
-
-
-# Get the roots by running BFS and making sure you can visit a good number of vertices
-"$GAPDIR"/sssp -f "$DDIR/$DATA.wel" -n $(($NRT*2)) > tmp.log
-# GAP output is, e.g. "took 382 iterations". We don't want it to take 1 iteration.
-awk -v NRT=$NRT '/Source/{src=$2}/took [0-9]+ iterations/{if($2>1 && cnt<NRT){printf "%d\n", src; cnt++}}' tmp.log > "$DDIR/$DATA-roots.v"
-rm tmp.log
-
 
 # GraphMat doesn't currently work
 # "$GRAPHMATDIR/bin/graph_converter" --selfloops 1 --duplicatededges 0 --inputformat 1 --outputformat 0 --inputheader 0 --outputheader 1 --edgeweighttype 1 --inputedgeweights 1 --outputedgeweights 1 "$DDIR/$DATA.wel" "$DDIR/$DATA.graphmat"
@@ -101,30 +81,28 @@ rm tmp.log
 # done
 
 echo Starting experiment at $(date)
+# TODO: for d in $DATA; do
+# ...
+# done
 # Run for GAP BFS
 # It would be nice if you could read in a file for the roots
 # Just do one trial to be the same as the rest of the experiments
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$GAPDIR"/bfs -r $ROOT -f "$DDIR/$DATA.wel" -n 1
+for ROOT in $(head -n $NRT "$DDIR/$d/${d}-roots.v"); do
+	"$GAPDIR"/bfs -r $ROOT -f "$DDIR/$d/${d}.wel" -n 1
 done
 
 # Run the GraphBIG BFS
 # For this, one needs a vertex.csv file and and an edge.csv.
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$GRAPHBIGDIR/benchmark/bench_BFS/bfs" --dataset "$DDIR/$DATA" --root $ROOT --threadnum $OMP_NUM_THREADS
-done
-
+head -n $NRT "$DDIR/$d/${d}-roots.v" > "$DDIR/$d/${d}-${NRT}roots.v"
+"$GRAPHBIGDIR/benchmark/bench_BFS/bfs" --dataset "$DDIR/$d" --rootfile "$DDIR/$d/${d}-${NRT}roots.v" --threadnum $OMP_NUM_THREADS
 
 # Run the GAP SSSP for each root
-for ROOT in $(head -n $NRT "$DDIR/$DATA.v"); do
-	"$GAPDIR"/sssp -r $ROOT -f "$DDIR/$DATA.wel" -n 1
+for ROOT in $(head -n $NRT "$DDIR/$d/${d}.v"); do
+	"$GAPDIR"/sssp -r $ROOT -f "$DDIR/$d/${d}.wel" -n 1
 done
 
 # Run the GraphBIG SSSP
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$GRAPHBIGDIR/benchmark/bench_shortestPath/sssp" --dataset "$DDIR/$DATA" --root $ROOT --threadnum $OMP_NUM_THREADS
-done
-
+"$GRAPHBIGDIR/benchmark/bench_shortestPath/sssp" --dataset "$DDIR/$d" --rootfile "$DDIR/$d/${d}-${NRT}roots.v" --threadnum $OMP_NUM_THREADS
 
 # Run PowerGraph SSSP
 if [ "$OMP_NUM_THREADS" -gt 64 ]; then
@@ -133,22 +111,22 @@ if [ "$OMP_NUM_THREADS" -gt 64 ]; then
 else
     export GRAPHLAB_THREADS_PER_WORKER=$OMP_NUM_THREADS
 fi
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$POWERGRAPHDIR/release/toolkits/graph_analytics/sssp" --graph "$DDIR/$DATA.wel" --format tsv --source $ROOT
+for ROOT in $(head -n $NRT "$DDIR/$d/${d}-roots.v"); do
+	"$POWERGRAPHDIR/release/toolkits/graph_analytics/sssp" --graph "$DDIR/$d/${d}.wel" --format tsv --source $ROOT
 done
 
 # PageRank Note: ROOT is a dummy variable to ensure the same # of trials
 # Run GAP PageRank
 # error = sum(|newPR - oldPR|)
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$GAPDIR"/pr -f "$DDIR/$DATA.wel" -i $MAXITER -t $TOL -n 1
+for ROOT in $(head -n $NRT "$DDIR/$d/${d}-roots.v"); do
+	"$GAPDIR"/pr -f "$DDIR/$d/${d}.wel" -i $MAXITER -t $TOL -n 1
 done
 
 # Run GraphBIG PageRank
 # The original GraphBIG has --quad = sqrt(sum((newPR - oldPR)^2))
 # GraphBIG error has been modified to now be sum(|newPR - oldPR|)
-for ROOT in $(head -n $NRT "$DDIR/$DATA-roots.v"); do
-	"$GRAPHBIGDIR/benchmark/bench_pageRank/pagerank" --dataset "$DDIR/$DATA" --maxiter $MAXITER --quad $TOL --threadnum $OMP_NUM_THREADS
+for ROOT in $(head -n $NRT "$DDIR/$d/${d}-roots.v"); do
+	"$GRAPHBIGDIR/benchmark/bench_pageRank/pagerank" --dataset "$DDIR/$d" --maxiter $MAXITER --quad $TOL --threadnum $OMP_NUM_THREADS
 done
 
 echo Finished experiment at $(date)
