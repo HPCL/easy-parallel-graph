552638807b588fd049fb87b9a761d8903bd21f55
Samuel Pollard <spollard@cs.uoregon.edu>
2017-07-30 02:08:52 -0700

    Overwrite log file in run-multi-thread.

--- experiment/run-multi-thread.sh
+++ experiment/run-multi-thread.sh
@@ -2,7 +2,7 @@
 # Runs run-experiment.sh for each thread number.
 # Must have been parsed beforehand.
 S=$1
-THREADS="1 2 4 8 12 16 20 24 28 32"
+THREADS="12 16 20 24 28 32"
 if [ -z "$S" ] || [ "$S" = '--help' ] || [ "$S" = '-h' ]; then
 	echo "usage: run-multi-thread.sh <scale>
 	You can also change the THREADS variable in this script"
@@ -10,6 +10,6 @@ if [ -z "$S" ] || [ "$S" = '--help' ] || [ "$S" = '-h' ]; then
 fi
 mkdir -p output/out-kron-$S
 for T in $(echo $THREADS); do
-	./run-experiment.sh $S $T >> output/kron-${S}-logs/${T}t.log 2>> output/kron-${S}-logs/${T}t.err
+	./run-experiment.sh $S $T > output/kron-${S}-logs/${T}t.log 2> output/kron-${S}-logs/${T}t.err
 done
 
