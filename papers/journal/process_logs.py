ecc0ccdc0b08b248a3ba072c39976754f3c55d1d
Boyana Norris <brnorris03@gmail.com>
2018-02-18 07:18:17 -0800

    updated data and processing script (this will be used for the next paper, too, so should probably move it somewhere else

--- papers/journal/process_logs.py
+++ papers/journal/process_logs.py
@@ -8,8 +8,8 @@ phases = {
     "Starting Classify and Insert" : "MST-Insert",
     "Starting process_deletions" : "MST-Delete",
     "INFO: CommandLine" : "Galois-All",
-    "STAT,(NULL),Time" : "DONE",
-    "Total Time for Updating" : "DONE",
+    "Command being timed:" : "Total memory",
+    "Exit status: 0" : "DONE"
 }
 
 sleep_power={1:35.59980, 2:32.50874, 4:39.00752, 8:27.53429, 16:29.59654, 24:26.78392,
@@ -20,49 +20,60 @@ sleep_energy={1:356.00497, 2:325.09772, 4:390.13837, 8:275.48913, 16:296.30119, 
 def usage():
     print("Usage: power_summarize.py <directory with log files>")
 
-networks = {}
+class Experiment:
+    def __init__(self,which,name,num=0):
+        global phases
+        self.which = which
+        self.name = name
+        self.num = num
+        self.time = {}
+        self.power = {}
+        self.energy = {}
+        self.memory = {}
+        for p in phases.values():
+            if p in ["DONE","Total memory"]: continue
+            self.time[p] = []
+            self.energy[p] = []
+            self.power[p] = []
+    
+    def set(self,num,which,phase,t,e,p,m):
+        if phase == "DONE": return
+        self.num = num
+        if phase == "Total memory":
+            self.memory[which + '-' + phase] = m
+        else:
+            #print("Extending",str(t),str(e),str(p))
+            self.time[phase]=t  # tuple with values for each CPU
+            self.energy[phase]=e # tuple with values for each CPU
+            self.power[phase]=p # tuple with values for each CPU
+
+    def __repr__(self):
+        buf = "Experiment %d;%s" % (self.num,self.name)
+        buf += "\n%s;Time(s):" % self.name + str(self.time)
+        buf += "\n%s;Energy(J):" % self.name + str(self.energy)
+        buf += "\n%s;Power(W):" % self.name + str(self.power)
+        buf += "\n%s;Memory(GB):" % self.name + str(self.memory)
+        return buf
+
 
 class Network:
     def __init__(self,paramstr):
         global phases,cpus
         self.id = paramstr
-        self.time = {}
-        self.power = {}
-        self.energy = {}
-        for pkg in [0,1]:
-            self.time[pkg] = {}
-            self.power[pkg] = {}
-            self.energy[pkg] = {}
-            for p in phases.values():
-                if p == "DONE": continue
-                self.time[pkg][p] = []
-                self.energy[pkg][p] = []
-                self.power[pkg][p] = []
+        self.experiments = []
 
-    def extendData(self,phase,t,e,p):
-        if phase == "DONE": return
-        #print("Extending",str(t),str(e),str(p))
-        for pkg in [0,1]:
-            self.time[pkg][phase].append(t[pkg])
-            self.energy[pkg][phase].append(e[pkg])
-            self.power[pkg][phase].append(p[pkg])
+    def extendData(self,experiment):
+        self.experiments.append(experiment)
 
-    def print(self):
-        print(self.id)
-        print(self.time)
-        print(self.energy)
-        print(self.power)
-    
     def __repr__(self):
-        buf = self.id + ':'
-        buf += "\nTime:" + str(self.time)
-        buf += "\nEnergy:" + str(self.energy)
-        buf += "\Power:" + str(self.power)
+        buf = "\n".join([str(exp) for exp in self.experiments]) + "\n"
         return buf
+    
+    def dump(self):
+        print(self.experiments)
 
 
-def processLog(logpath):
-    global networks
+def processLog(logpath,networks):
     # For example, output-8epv/mst-248_ER_100i_1000000_16t.log
     log = os.path.basename(logpath).split('.')[0]
     ind = log.find('-')
@@ -75,32 +86,50 @@ def processLog(logpath):
     # Examples of (which, params):
     # mst ['248', 'G', '100i', '1000000', '64t']
     # galois ['248', 'ER', '100i', '1000000', '4t']
-    phase = None; time = [None]*2; energy=[None]*2; power=[None]*2
-    lines = open(logpath,'r').readlines()
+    phase = None; time = [None]*2; energy=[None]*2; power=[None]*2; memory=0
+    contents = open(logpath,'r').read()
+    if contents.find("Command being timed:") < 0:
+        phases["STAT,(NULL),Time"] = "DONE"
+        phases["Total Time for Updating"] = "DONE"
+    lines = contents.split('\n')
+    new = True; num =0
     for line in lines:
+        if new:
+            experiment = Experiment(which,paramstr,num)
+            new = False
         line = line.strip()
         if line.startswith("Found"): continue
-            #if line.startswith("Time Taken for Initializing") or
-            #line.startswith("Galois Benchmark Suite"): phase = None
+        if line.startswith("Time Taken for Initializing") or \
+           line.startswith("Galois Benchmark Suite"):
+            phase = None
+            new = True
         s = line.split()
         
         for p in phases.keys():
             #print(p,line)
             if line.startswith(p):
-                if phase: networks[paramstr].extendData(phase,time,energy,power)
+                if phase: experiment.set(num,which,phase,time,energy,power,memory)
                 phase = phases[p]  # new phase
-                time = [None]*2; energy=[None]*2; power=[None]*2
+                time = [None]*2; energy=[None]*2; power=[None]*2; memory=0
                 #print("New phase: ",phase)
                 break
 
-
-        if phase == "DONE": phase = None
         if not phase: continue
+        if phase == "Total memory":
+            if line.find("Maximum resident set size")>=0:
+                memory = float(line.split(":")[-1].strip())/1048576.0
+
+        if phase == "DONE":
+            phase = None
+            if experiment.time['MST-Insert'] or experiment.time['Galois-All']:
+                networks[paramstr].extendData(experiment)
+                num += 1
+            
+
         if line.find("rapl:::PACKAGE_ENERGY:PACKAGE") < 0: continue
+
         # See more complete example RAPL output at bottom of file
         # 2.9492 s 195.8815 (* Total Energy for rapl:::PACKAGE_ENERGY:PACKAGE0 *)
-
-        # Get triplets: time, total energy, average power
         if phase and line.find("rapl:::PACKAGE_ENERGY:PACKAGE") > 0:
             #print(logpath,line)
             pkg = int(s[-2][-1])
@@ -111,8 +140,18 @@ def processLog(logpath):
                 power[pkg]=float(s[2])
 
 
-def processLogs():
+def processLogs(logdir):
     global phases
+    networks = {}
+    logfiles = glob.glob(logdir+"/*.log")
+    for logpath in logfiles:
+        processLog(logpath,networks)
+    return networks
+
+def combine():
+    pass
+
+def main():
     if len(sys.argv) < 2:
         usage()
         sys.exit(1)
@@ -122,14 +161,10 @@ def processLogs():
         usage()
         sys.exit(1)
 
-    logfiles = glob.glob(logdir+"/*.log")
-    for logpath in logfiles:
-        processLog(logpath)
-
-def main():
-    processLogs()
+    networks = processLogs(logdir)
     for n in networks.values():
-        n.print()
+        print(n)
+        #n.dump()
         pass
 
 
