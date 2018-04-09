4085ff41890338b8e4d5f3022893b23b25b8c41d
Sudharshan <srini.sudharshan1@gmail.com>
2018-04-08 18:03:35 -0700

    updated without networkx

--- learn/vertex_convert.py
+++ learn/vertex_convert.py
@@ -42,8 +42,10 @@ for i in range(0,len(g)):
 	g[i][0]=rename[g[i][0]]
 	g[i][1]=rename[g[i][1]]
 
-G=nx.from_edgelist(g)			#convert to networkx
-nx.write_edgelist(G, "new_graph.txt" ,data = False) #write to file
+#G=nx.from_edgelist(g)			#convert to networkx
+#nx.write_edgelist(G, "new_graph.txt" ,data = False) #write to file
+with open('new_graph.txt', 'w') as file:
+    file.writelines('\t'.join(str(j) for j in i) + '\n' for i in g)
 
 
 
