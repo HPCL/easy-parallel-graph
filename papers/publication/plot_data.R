247a2bc8bb5adbcc63025a34f727108d27e270bf
Samuel Pollard <sam.d.pollard@gmail.com>
2018-06-13 16:28:24 -0700

    Upgraded realworld figure and fixed small bug. Added overview, but now writing is taking place in another repository

--- papers/publication/plot_data.R
+++ papers/publication/plot_data.R
@@ -1,28 +1,96 @@
 # Generate some plots using the data parsed by ../../misc/parse-output.sh
 # Make this section be the results from the optimal number of threads
+library(ggplot2)
+prefix <- "results"
+outdir <- "graphics_new"
 
+# Doesn't quite work
+# plot_value <- function(x, algo, metric, main_title, ylabel="Time (seconds)") {
+# 	df <- subset(x, x$Algo == algo & x$Metric == metric,
+# 			c("Sys","Time"))
+# 	df$Sys <- factor(df$Sys, ordered=TRUE)
+# 	df <- df[order(df$Sys),]
+# 	boxplot(Time~Sys, df, ylab = ylabel,
+# 			main = main_title, col=bpc, xaxt = "n", xlab = "")
+# 	axis(1, labels=FALSE)
+# 	ax_lab <- as.character(unique(df$Sys))
+# 	text(x = seq_along(ax_lab), y = par("usr")[3]*1.1,
+# 			labels = ax_lab, srt = 30, adj = c(0.95,0.95),
+# 			xpd = TRUE, cex = 1.0)
+# 	mtext(paste0("Scale = ",scale, ", nedges = ",
+# 			prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
+# }
+
+avgs <- tabulate_data <- function(thr, dataset)
+{
+	filename <- paste0(prefix,"/","parsed-",dataset,"-",thr,".csv")
+	x <- read.csv(filename, header = FALSE)
+	colnames(x) <- c("Sys","Algo","Metric","Time")
+	avgs <- aggregate(x$Time, list(x$Sys, x$Algo, x$Metric), mean)
+	return(avgs)
+}
+
+compare_datasets <- function(all_avgs, dataset_list, metric = "Time")
+{
+	# If you just want a single algorithm, remove the "facet_wrap" line and use
+	# avgs.m.a <- subset(all_avgs, all_avgs$Metric==metric & all_avgs$Algo==algo)
+	avgs.m <- subset(all_avgs, all_avgs$Metric==metric)
+	p <- ggplot(avgs.m, aes(Dataset, Time, fill = Sys)) +
+			geom_bar(stat = "identity", position = "dodge") +
+			facet_wrap(~Algo, scales = "free") +
+			scale_fill_brewer(palette="Accent") +
+			# If your datasets are too long you can rename them
+			scale_x_discrete(labels = c("dota","Patents")) +
+			theme(axis.text.x = element_text(angle = 30, hjust = 1))
+	outfn <- paste0(outdir,"/","compare-",metric,".pdf")
+	message("Writing to", outfn)
+	pdf(outfn, width = 5, height = 5)
+	print(p)
+	dev.off()
+}
 
 ###
-# Part 1: Generate the plots for a single problem size and multiple algorithms
+# Part 1: Realworld datasets
 ###
+dataset_list <- c("cit-Patents", "dota-league")
+thr <- 32 # Just start with one thread count
+all_avgs <- data.frame()
+for (dset in dataset_list) {
+	avgs <- tabulate_data(thr, dset)
+	all_avgs <- rbind(all_avgs, cbind(dset, avgs))
+}
+colnames(all_avgs) <- c("Dataset","Sys","Algo","Metric","Time")
+compare_datasets(all_avgs, dataset_list)
+
+###
+# Part 2: Generate the plots for a single problem size and multiple algorithms
+###
+nvertices <- 16
 scale <- 22
+nedges <- nvertices * 2^scale
 bpc <- "cyan"
-filename <- paste0("parsed-kron-",scale,"-32.csv")
+filename <- paste0(prefix,"/","parsed-kron-",scale,"-32.csv")
 x <- read.csv(filename, header = FALSE)
 colnames(x) <- c("Sys","Algo","Metric","Time")
 
 bfs_time <- subset(x, x$Algo == "BFS" & x$Metric == "Time",
 		c("Sys","Time"))
+bfs_time$Sys <- factor(bfs_time$Sys, ordered=TRUE)
+bfs_time <- bfs_time[order(bfs_time$Sys),]
+
 bfs_dsc <- subset(x, x$Algo == "BFS" & x$Metric == "Data structure build",
 		c("Sys","Time"))
-bfs_dsc$Sys <- factor(bfs_dsc$Sys)
+bfs_dsc$Sys <- factor(bfs_dsc$Sys, ordered=TRUE)
+bfs_dsc <- bfs_dsc[order(bfs_dsc$Sys),]
 
 sssp_time <- subset(x, x$Algo == "SSSP" & x$Metric == "Time",
 		c("Sys","Time"))
 sssp_dsc <- subset(x, x$Algo == "SSSP" & x$Metric == "Data structure build",
 		c("Sys","Time"))
-sssp_time$Sys <- factor(sssp_time$Sys)
-sssp_dsc$Sys <- factor(sssp_dsc$Sys)
+sssp_time$Sys <- factor(sssp_time$Sys, ordered=TRUE)
+sssp_time <- sssp_time[order(sssp_time$Sys),]
+sssp_dsc$Sys <- factor(sssp_dsc$Sys, ordered=TRUE)
+sssp_dsc <- sssp_dsc[order(sssp_dsc$Sys),]
 # In the paper we compare range(sssp_time$Time) and range(bfs_time$Time)
 
 pr_time <- subset(x, x$Algo == "PageRank" & x$Metric == "Time",
@@ -31,34 +99,47 @@ pr_dsc <- subset(x, x$Algo == "PageRank" & x$Metric == "Data structure build",
 		c("Sys","Time"))
 pr_iters <- subset(x, x$Algo == "PageRank" & x$Metric == "Iterations",
 		c("Sys","Time"))
-pr_time$Sys <- factor(pr_time$Sys)
-pr_dsc$Sys <- factor(pr_dsc$Sys)
+pr_time$Sys <- factor(pr_time$Sys, ordered=TRUE)
+pr_time <- pr_time[order(pr_time$Sys),]
+pr_dsc$Sys <- factor(pr_dsc$Sys, ordered=TRUE)
+pr_dsc <- pr_dsc[order(pr_dsc$Sys),]
 
-pdf("graphics/bfs_time.pdf", width = 5.2, height = 5.2)
+pdf(paste0(outdir,"/","bfs_time.pdf"), width = 5.2, height = 5.2)
 boxplot(Time~Sys, bfs_time, ylab = "Time (seconds)",
-		main = "BFS Time", log = "y", col=bpc)
-mtext(paste0("Scale = ",scale), side = 3)
+		main = "BFS Time", col=bpc, xaxt = "n", xlab = "")
+axis(1, labels=FALSE)
+ax_lab <- as.character(unique(bfs_time$Sys))
+text(x = seq_along(ax_lab),
+		y = par("usr")[3]*1.1,
+		labels = ax_lab,
+		srt = 30,
+		adj = c(0.95,0.95),
+		xpd = TRUE,
+		cex = 1.0)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 dev.off()
 
-pdf("graphics/bfs_dsc.pdf", width = 5.2, height = 5.2)
+pdf(paste0(outdir,"/","bfs_dsc.pdf"), width = 5.2, height = 5.2)
 boxplot(Time~Sys, bfs_dsc, ylab = "Time (seconds)",
 		main = "BFS Data Structure Construction", col=bpc, log = "y")
-mtext(paste0("Scale = ",scale), side = 3)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 dev.off()
 
-pdf("graphics/sssp_time.pdf", width = 5.5, height = 4.5)
-boxplot(Time~Sys, sssp_time, ylab = "Time (seconds)",
+pdf(paste0(outdir,"/","sssp_time.pdf"), width = 5.5, height = 4.5)
+bp <- boxplot(Time~Sys, sssp_time, ylab = "Time (seconds)",
 		main = "SSSP Time", log = "y", col=bpc)
-mtext(paste0("Scale = ",scale), side = 3)
+text(bp, par("usr")[3], labels = sssp_time[[1]], srt = 30,
+		adj = c(0.95,0.95), xpd = TRUE, cex = 1.0)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 dev.off()
 
-pdf("graphics/sssp_dsc.pdf", width = 3.5, height = 4.5)
+pdf(paste0(outdir,"/","sssp_dsc.pdf"), width = 3.5, height = 4.5)
 boxplot(Time~Sys, sssp_dsc, ylab = "Time (seconds)",
 		main = "SSSP Data Structure\nConstruction", log = "y", col=bpc)
 # mtext(paste0("Scale = ",scale), side = 3)
 dev.off()
 
-pdf("graphics/pr_iters.pdf", width = 4, height = 4)
+pdf(paste0(outdir,"/","pr_iters.pdf"), width = 4, height = 4)
 pr_mean_iters <- aggregate(pr_iters$Time, list(pr_iters$Sys), mean)
 pr_sys_order <- order(pr_mean_iters[[2]])
 pr_mean_iters <- pr_mean_iters[pr_sys_order,]
@@ -68,7 +149,7 @@ text(bp, par("usr")[3], labels = pr_mean_iters[[1]], srt = 30,
 		adj = c(0.95,0.95), xpd = TRUE, cex = 1.0)
 dev.off()
 
-pdf("graphics/pr_time.pdf", width = 4, height = 4)
+pdf(paste0(outdir,"/","pr_time.pdf"), width = 4, height = 4)
 pr_sys_labels <- pr_mean_iters[[1]] # Get the order from iterations
 pr_time$Sys <- factor(pr_time$Sys, pr_sys_labels, ordered = TRUE)
 pr_time <- pr_time[order(pr_time$Sys),]
@@ -78,7 +159,7 @@ boxplot(Time~Sys, pr_time, ylab = "Time (seconds)",
 		xaxt = "n", xlab = "")
 		#names.arg = pr_sys)
 axis(1, labels = FALSE)
-mtext(paste0("Scale = ",scale), side = 3)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 text(x = seq(pr_sys), y = par("usr")[3]+0.90,
 		srt = 30, adj = c(1,2), xpd = TRUE,
 		labels = pr_sys, cex = 1.0)
@@ -86,14 +167,15 @@ dev.off()
 
 
 ###
-# Part 2: Generate the plots for a single algorithm and multiple problem sizes
+# Part 3: Generate the plots for a single algorithm and multiple problem sizes
 ###
 threadcnts <- c(1,2,4,8,16,32,64,72)
-scale <- 20
+scale <- 22
+nedges <- nvertices * 2^scale
 measure_scale <- function(algo, scale) {
 	# Read in and average the data for BFS for each thread
 	# It is wasteful to reread the parsed*-1.csv but it simplifies the code
-	x <- read.csv(paste0("parsed-kron-",scale,"-1.csv"), header = FALSE)
+	x <- read.csv(paste0(prefix,"/","parsed-kron-",scale,"-1.csv"), header = FALSE)
 	colnames(x) <- c("Sys","Algo","Metric","Time")
 	x$Sys <- factor(x$Sys, ordered = TRUE)
 	systems <- unique(subset(x$Sys, x$Algo == algo, c("Sys")))
@@ -103,7 +185,7 @@ measure_scale <- function(algo, scale) {
 	colnames(algo_time) <- threadcnts
 	for (ti in seq(length(threadcnts))) {
 		thread <- threadcnts[ti]
-		Y <- read.csv(paste0("parsed-kron-",scale,"-",thread,".csv"),
+		Y <- read.csv(paste0(prefix,"/","parsed-kron-",scale,"-",thread,".csv"),
 				header = FALSE)
 		ti_time <- subset(Y, Y[[2]] == algo & Y[[3]] == "Time",
 				c(V1,V4))
@@ -125,7 +207,7 @@ for (ti in rev(seq(length(threadcnts)))) {
 
 # Plot the strong scalability for BFS
 # Actually, this is called Parallel Efficiency!
-pdf("graphics/bfs_ss.pdf", width = 7, height = 4)
+pdf(paste0(outdir,"/","bfs_ss.pdf"), width = 7, height = 4)
 systems <- rownames(bfs_ss)
 plot(as.numeric(bfs_ss[1,]), xaxt = "n", type = "b", ylim = c(0,1),
 		ylab = "", xlab = "Threads", col = colors[1],
@@ -142,13 +224,13 @@ legend(legend = c("Linear", rownames(bfs_ss)), x = "topright",
 		box.lwd = 1, lwd = c(2, rep(3,length(systems))),
 		col = c("#000000FF", colors),
 		bg = "white")
-mtext(paste("Scale =", scale, "edges =", 16*2^scale), side = 3)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 mtext(expression(italic(over(T[1],n*T[n]))),
 		side = 2, las = 1, xpd = NA, outer = TRUE, adj = -0.2)
 dev.off()
 
 # Plot the speedup for BFS
-pdf("graphics/bfs_speedup.pdf", width = 7, height = 4)
+pdf(paste0(outdir,"/","bfs_speedup.pdf"), width = 7, height = 4)
 bfs_spd <- data.frame(t(apply(bfs_ss, 1, function(x){x*threadcnts})))
 colnames(bfs_spd) <- threadcnts
 plot(as.numeric(bfs_spd[1,]), xaxt = "n", type = "b", ylim = c(1,18),
@@ -160,20 +242,21 @@ for (pli in seq(2,nrow(bfs_ss))) {
 }
 lines(1:length(threadcnts), threadcnts, lwd = 1, col = "#000000FF")
 axis(1, at = seq(length(threadcnts)), labels = threadcnts)
-mtext(paste0("Scale = ", scale), side = 3)
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 legend(legend = c("Linear", rownames(bfs_spd)), x = "topleft", bg = "white",
 		col = c("#000000FF", colors), lwd = c(1,rep(3,length(systems))),
 		lty = c(1:length(systems), 1), pch = c(NA_integer_, 1:length(systems)))
 dev.off()
 
 ###
-# Part 3: Power
+# Part 4: Power
 ###
 # Just BFS for now.
 # Read in the data
 GRAPH500NRT <- 64 # Even though everyone else does 32.
-scale <- 22
-filename <- paste0("parsed",scale,"-32-power.csv")
+scale <- 23
+nedges <- nvertices * 2^scale
+filename <- paste0(prefix,"/","parsed",scale,"-32-power.csv")
 x <- read.csv(filename, header = FALSE)
 colnames(x) <- c("Sys","Algo","Metric","Value")
 bfs_cpu_pwr <- subset(x, x$Algo == "BFS" & x$Metric == "Average CPU Power (W)",
@@ -232,7 +315,7 @@ print(paste0("Increase over sleep & ",
 		intersperse(bfs_cpu_nrg_per_root/sleep_nrg_per_root, " & ")))
 
 # This plot doesn't convey the data quite right...
-# pdf("graphics/bfs_cpu_energy.pdf", width = 4.5, height = 4.5)
+# pdf(paste0(outdir,"/","bfs_cpu_energy.pdf"), width = 4.5, height = 4.5)
 # bfs_cpu_nrg_mat <- matrix(
 # 		c(sleep_nrg_per_root, bfs_cpu_nrg_per_root), # -sleep_nrg_per_root),
 # 		nrow = 2, ncol = length(bfs_systems), byrow = TRUE)
@@ -247,31 +330,31 @@ print(paste0("Increase over sleep & ",
 # dev.off()
 
 # Make some plots
-pdf("graphics/bfs_cpu_power.pdf", width = 5.2, height = 5.2)
+pdf(paste0(outdir,"/","bfs_cpu_power.pdf"), width = 5.2, height = 5.2)
 boxplot(Value~Sys, bfs_cpu_pwr, ylab = "Average Power (Watts)",
 		col="yellow", ylim=c(cpu_pwr_sleep$Value*0.9, max(bfs_cpu_pwr$Value)))
 title(main = "CPU Average Power Consumption During BFS")
 abline(mean(cpu_pwr_sleep$Value), 0, col = "orangered", lwd = 2)
-mtext(paste0("Scale = ",scale), side = 3) # May want to remove subtitle later
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 legend(legend = c("sleep"), x = "bottomright", inset = c(0,0),
 		lty = c(1), lwd = 2, col = "orangered", bg = "white")
 dev.off()
 
-pdf("graphics/bfs_ram_power.pdf", width = 5.2, height = 5.2)
+pdf(paste0(outdir,"/","bfs_ram_power.pdf"), width = 5.2, height = 5.2)
 boxplot(Value~Sys, bfs_ram_pwr, ylab = "Average Power (Watts)",
 		col="yellow", ylim=c(ram_pwr_sleep$Value*0.9, max(bfs_ram_pwr$Value)))
 title(main = "RAM Power Consumption During BFS",
 		sub = paste0("Scale = ",scale)) # May want to remove subtitle later
 abline(mean(ram_pwr_sleep$Value), 0, col = "orangered", lwd = 2)
-mtext(paste0("Scale = ",scale), side = 3) # May want to remove subtitle later
+mtext(paste0("Scale = ",scale, ", nedges = ",prettyNum(nedges,big.mark=",",scientific=FALSE)), side = 3)
 legend(legend = c("sleep"), x = "bottomright", inset = c(0,0),
 		lty = c(1), lwd = 2, col = "orangered", bg = "white")
 dev.off()
 
 ###
-# Part 4: Dota-league dataset with easy-parallel-graph
+# Part 5: Dota-league dataset with easy-parallel-graph
 ###
-dota <- read.csv("parseddota-32-redo.csv", header = FALSE)
+dota <- read.csv(prefix,"/","parseddota-32-redo.csv", header = FALSE)
 colnames(dota) <- c("Sys","Algo","Metric","Time")
 dota$Sys <- factor(dota$Sys, ordered = TRUE)
 systems <- levels(subset(x$Sys, x$Algo == algo, c("Sys")))
@@ -279,7 +362,7 @@ systems <- levels(subset(x$Sys, x$Algo == algo, c("Sys")))
 dota_means <- aggregate(dota$Time, list(dota$Sys, dota$Algo, dota$Metric), mean)
 
 ###
-# Part 5: Supplementary statistics used in the paper's prose.
+# Part 6: Supplementary statistics used in the paper's prose.
 ###
 sd(pr_time$Time[pr_time$Sys == "PowerGraph"]) /
 		mean(pr_time$Time[pr_time$Sys == "PowerGraph"])
